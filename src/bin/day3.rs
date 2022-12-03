1b9f65c34dbaf1c9d726b37becc43b007000907a
Cursedevitili <51824354+Cursedevitili@users.noreply.github.com>
2022-12-03 21:30:33 +0200

    Not building day 2 part 2 in progress

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -14,8 +14,25 @@ struct Rucksack{
     wrong_items: Vec<WrongItem>
 }
 
-fn calc_item_priority(item: char) {
-    println!("{}", item.to_string())
+static abc:&'static str = "abcdefghijklmnopqrstuvwxyz";
+static ABC:&'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+fn calc_item_priority(item: &char) -> i32 {
+    let mut how_many = 1;
+    for i in abc.chars() {
+        if i == *item {
+            return how_many;
+        }
+        how_many += 1;
+    }
+    for i in ABC.chars() {
+        if i == *item {
+            return how_many;
+        }
+        how_many += 1;
+    }
+
+    how_many
 }
 
 impl Rucksack{
@@ -30,34 +47,63 @@ impl Rucksack{
     }
 
     fn parse_wrong_items(&mut self) {
-        for (_, item1) in self.comp1.char_indices() {
-            for (_, item2) in self.comp2.char_indices() {
-                if item1 == item2 {
-                    calc_item_priority(item1);
+        let mut count = 0;
+        for i in self.comp1.chars() {
+            for j in self.comp2.chars() {
+                if i == j {
+                    let prio = calc_item_priority(&i);
+                    // println!("Found one: {}", i);
                     let item_to_add = WrongItem{
-                        item_type: item1.to_string(),
-                        item_priority: 3
+                        item_type: i.to_string(),
+                        item_priority: prio
                     };
 
                     let mut vector = &mut self.wrong_items;
-                    vector.push(item_to_add);
+                    let mut add_here = true;
+                    for item in &self.wrong_items {
+                        if item.item_type == item_to_add.item_type {
+                            add_here = false;
+                        }
+                    }
+                    if add_here {
+                        &mut self.wrong_items.push(item_to_add);
+                    }
                 }
             }
         }
     }
 }
 
+fn process_groups_three(group: Vec<Rucksack>) {
+    for sack in group {
+
+    }
+}
+
 fn main() {
-    let input = shared::load_input("day3demo.txt").unwrap_or_else(
+    let input = shared::load_input("day3demo2.txt").unwrap_or_else(
         |e| {
             println!("{}", e);
             std::process::exit(1)
         }
     );
 
+    let mut priority_sum = 0;
+    let mut output = String::from("");
+
+    let mut rucksackgroup: Vec<Rucksack> = Vec::new();
     for line in input.lines() {
         let mut rucksack = Rucksack::new(&line);
         rucksack.parse_wrong_items();
-        // println!("{:?}", rucksack);
+        if rucksackgroup.len() < 3 {
+            rucksackgroup.push(rucksack.clone());
+        } else {
+            rucksackgroup.clear()
+        }
+        println!("{:?}", rucksack);
+        let wrong_item_sum: i32 = rucksack.wrong_items.iter().map(|x| x.item_priority).sum();
+        priority_sum += wrong_item_sum;
     }
+    output.push_str(&format!("Sum of priorities: {}", priority_sum));
+    shared::write_output("day3output.txt",&output);
 }
