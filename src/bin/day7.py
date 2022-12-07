71d7aa38fb629065329fa45ab14be7e184338b27
Cursedevitili <51824354+Cursedevitili@users.noreply.github.com>
2022-12-07 22:19:03 +0200

    Day 7 part 2 done.

--- src/bin/day7.py
+++ src/bin/day7.py
@@ -14,6 +14,7 @@ class Folder:
         self.folders: [Folder] = []
         self.upper: Folder = None
         self.level = level
+        self.size_if_deleted: int = None
 
     def add_file(self, name, size):
         self.files.append(File(name, size))
@@ -39,21 +40,24 @@ class Folder:
             files_sum += folder.find_files_sum_recurring(files_sum)
         return files_sum
 
-    def recur_find_folders_under_100_000(self, start: int, total_size: int) -> (int, int):
+    def recur_find_folders_under_100_000(self, start: int, total_size: int, folders: list["Folder"]) -> (int, int, list["Folder"]):
         files_sum = sum(list(map(lambda n: n.size, self.files)))
         size_deeper = 0
         for folder in self.folders:
             size_deeper += folder.find_files_sum_recurring(size_deeper)
 
+        self.size_if_deleted = files_sum + size_deeper
+        folders.append(self)
+
         total_size = total_size
-        print(f"Dir {self.name} files sum: {files_sum} + inderect: {size_deeper}, sum is {files_sum+size_deeper}")
+        # print(f"Dir {self.name} files sum: {files_sum} + inderect: {size_deeper}, sum is {files_sum+size_deeper}")
         if files_sum + size_deeper <= 100000:
             start += 1
             total_size += files_sum + size_deeper
 
         fold: Folder
         for fold in self.folders:
-            (start, total_size) = fold.recur_find_folders_under_100_000(start, total_size)
+            (start, total_size) = fold.recur_find_folders_under_100_000(start, total_size, folders)
 
         return start, total_size
 
@@ -105,8 +109,30 @@ def main():
             line_counter += 1
 
     print(file_structure)
-    result, total_size = file_structure.recur_find_folders_under_100_000(0, 0)
-    print(f"Folder sizes under 100000: {result}, summed size: {total_size}")
+    folder_list = []
+    result, total_size = file_structure.recur_find_folders_under_100_000(0, 0, folder_list)
+    out1 = f"Folder sizes under 100000: {result}, summed size: {total_size}"
+    print(out1)
+
+    def sort_func(e: Folder):
+        return e.size_if_deleted
+    folder_list.sort(key=sort_func)
+
+    files_sum = sum(list(map(lambda n: n.size_if_deleted, folder_list)))
+    total_free_disk_space = 70000000 - file_structure.size_if_deleted
+    still_needed = 30000000 - total_free_disk_space
+    done = False
+    for folder in folder_list:
+        if folder.size_if_deleted >= still_needed and not done:
+            out2 = f"Name: {folder.name} size if deleted: {folder.size_if_deleted} is enough"
+            print(out2)
+            done = True
+
+    with open('./../../output/day7output.txt', 'w') as f:
+        f.write(out1+"\n")
+        f.write(out2)
+
+
 
 if __name__ == "__main__":
     main()
