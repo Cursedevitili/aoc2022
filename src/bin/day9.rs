5c174b10be9c11b2c6f5845f4e59a7a4287387d3
Cursedevitili <51824354+Cursedevitili@users.noreply.github.com>
2022-12-09 20:06:54 +0200

    Day 9 part 1 done.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -2,66 +2,133 @@ struct Grid {
     map_len_h: i32,
     map_len_v: i32,
     head_pos: (i32, i32),
-    tail_pos: (i32, i32)
+    tail1_pos: (i32, i32),
+    tail2_pos: (i32, i32),
+    tail3_pos: (i32, i32),
+    tail4_pos: (i32, i32),
+    tail5_pos: (i32, i32),
+    tail6_pos: (i32, i32),
+    tail7_pos: (i32, i32),
+    tail8_pos: (i32, i32),
+    tail9_pos: (i32, i32),
+    tail_positions: Vec<(i32, i32)>,
 }
 
+#[derive(Debug)]
 enum Dir {
-    UP(u32),
-    DOWN(u32),
-    LEFT(u32),
-    RIGHT(u32)
+    UP(i32),
+    DOWN(i32),
+    LEFT(i32),
+    RIGHT(i32),
 }
 
 impl Dir {
-    fn parse_from_str(dir: &str, how_many: u32) -> Result<Dir, ()>{
+    fn build(dir: char, how_many: i32) -> Result<Dir, ()> {
         match dir {
-            "R" => Ok(Dir::RIGHT(how_many)),
-            "L" => Ok(Dir::LEFT(how_many)),
-            "U" => Ok(Dir::UP(how_many)),
-            "D" => Ok(Dir::DOWN(how_many)),
-            &_ => {Err(())}
+            'R' => Ok(Dir::RIGHT(how_many)),
+            'L' => Ok(Dir::LEFT(how_many)),
+            'U' => Ok(Dir::UP(how_many)),
+            'D' => Ok(Dir::DOWN(how_many)),
+            _ => { Err(()) }
         }
     }
 }
 
 impl Grid {
-    fn new () -> Grid {
-        Grid{
-            map_len_h: 6,
-            map_len_v: 6,
-            head_pos: (5,0),
-            tail_pos: (5,0)
+    fn new() -> Grid {
+        Grid {
+            map_len_h: 15,
+            map_len_v: 15,
+            head_pos: (4, 0),
+            tail1_pos: (4, 0),
+            tail2_pos: (4, 0),
+            tail3_pos: (4, 0),
+            tail4_pos: (4, 0),
+            tail5_pos: (4, 0),
+            tail6_pos: (4, 0),
+            tail7_pos: (4, 0),
+            tail8_pos: (4, 0),
+            tail9_pos: (4, 0),
+            tail_positions: vec![(5, 0)],
         }
     }
 
     fn calc_len(&self) -> i32 {
-        if self.head_pos == self.tail_pos {
+        if self.head_pos == self.tail1_pos {
             return 0;
         }
 
-        if self.head_pos.0 == self.tail_pos.0 {
-            return (self.head_pos.1 - self.tail_pos.1).abs()-1
-        }
-        else if self.head_pos.1 == self.tail_pos.1 {
-            return (self.head_pos.0 - self.tail_pos.0).abs()-1
+        if self.head_pos.0 == self.tail1_pos.0 {
+            return (self.head_pos.1 - self.tail1_pos.1).abs() - 1;
+        } else if self.head_pos.1 == self.tail1_pos.1 {
+            return (self.head_pos.0 - self.tail1_pos.0).abs() - 1;
         }
 
-        let y_diff = (self.head_pos.0 - self.tail_pos.0).abs();
-        let x_diff = (self.head_pos.1 - self.tail_pos.1).abs();
+        let y_diff = (self.head_pos.0 - self.tail1_pos.0).abs();
+        let x_diff = (self.head_pos.1 - self.tail1_pos.1).abs();
         x_diff + y_diff - 2
     }
 
-    fn print_state(self){
-        let mut map:Vec<Vec<char>> = vec![];
-        for x in 0.. self.map_len_v {
+    fn move_head(&mut self, dir: Dir) {
+        match dir {
+            Dir::UP(x) => {
+                for _ in 0..x {
+                    let head_last_pos = self.head_pos;
+                    self.head_pos = (self.head_pos.0 - 1, self.head_pos.1);
+                    self.move_tail(head_last_pos);
+                    self.print_state();
+                }
+            }
+            Dir::DOWN(x) => {
+                for _ in 0..x {
+                    let head_last_pos = self.head_pos;
+                    self.head_pos = (self.head_pos.0 + 1, self.head_pos.1);
+                    self.move_tail(head_last_pos);
+                    self.print_state();
+                }
+            }
+            Dir::LEFT(x) => {
+                for _ in 0..x {
+                    let head_last_pos = self.head_pos;
+                    self.head_pos = (self.head_pos.0, self.head_pos.1 - 1);
+                    self.move_tail(head_last_pos);
+                    self.print_state();
+                }
+            }
+            Dir::RIGHT(x) => {
+                for _ in 0..x {
+                    let head_last_pos = self.head_pos;
+                    self.head_pos = (self.head_pos.0, self.head_pos.1 + 1);
+                    self.move_tail(head_last_pos);
+                    self.print_state();
+                }
+            }
+        }
+    }
+
+    fn move_tail(&mut self, head_last_pos: (i32, i32)) {
+        let h_t_diff = self.calc_len();
+        if h_t_diff == 1 {
+            self.tail1_pos = head_last_pos;
+            if !self.tail_positions.contains(&self.tail1_pos) {
+                self.tail_positions.push(self.tail1_pos);
+            }
+        }
+    }
+
+    fn print_state(&self) {
+        let mut map: Vec<Vec<char>> = vec![];
+        for x in 0..self.map_len_v {
             map.push(vec![]);
         }
-        for i in 0 .. self.map_len_h {
-            for j in 0 .. self.map_len_v {
-                if self.head_pos == (i,j) {
+        for i in 0..self.map_len_h {
+            for j in 0..self.map_len_v {
+                if self.head_pos == (i, j) {
                     map[i as usize].push('H');
-                } else if self.tail_pos == (i,j) {
+                } else if self.tail1_pos == (i, j) {
                     map[i as usize].push('T');
+                } else if (i, j) == (4, 0) {
+                    map[i as usize].push('S');
                 } else {
                     map[i as usize].push('.')
                 }
@@ -77,9 +144,73 @@ impl Grid {
         }
         println!("{}", state);
     }
+
+    fn print_tail_visited(&mut self) {
+        let mut map: Vec<Vec<char>> = vec![];
+        for x in 0..self.map_len_v {
+            map.push(vec![]);
+        }
+        for i in 0..self.map_len_h {
+            for j in 0..self.map_len_v {
+                if self.tail_positions.contains(&(i, j)) {
+                    map[i as usize].push('#')
+                } else if (i, j) == (4, 0) {
+                    map[i as usize].push('S');
+                } else {
+                    map[i as usize].push('.')
+                }
+            }
+        }
+
+        let mut final_state = String::new();
+        for i in map {
+            for j in i {
+                final_state.push(j);
+            }
+            final_state.push('\n');
+        }
+        println!("{}", final_state);
+    }
 }
 
 fn main() {
+    let input = aoc2022::shared::load_input("day9.txt").unwrap_or_else(
+        |e| {
+            println!("{}", e);
+            std::process::exit(1)
+        }
+    );
+    let mut commands: Vec<Dir> = vec![];
+    for line in input.lines() {
+        let mut iter = line.chars();
+        let dir_char = iter.next().unwrap();
+        iter.next();
+        let many: i32 = iter.next()
+            .unwrap()
+            .to_string()
+            .parse()
+            .unwrap();
+        let last_possible = iter.next();
+        match last_possible {
+            None => { commands.push(Dir::build(dir_char, many).unwrap_or_else(|_| panic!())) }
+            Some(x) => {
+                let mut num_string = many.to_string();
+                num_string.push(x);
+                commands.push(Dir::build(dir_char, num_string.parse().unwrap()).unwrap_or_else(|_| panic!()));
+            }
+        }
+    }
+
     let mut grid = Grid::new();
-    grid.print_state();
+    for cmd in commands {
+        println!("{:?}", cmd);
+        grid.move_head(cmd);
+        // grid.print_state();
+        println!();
+    }
+    grid.print_tail_visited();
+    let tails_pos_len = grid.tail_positions.len();
+    let mut out1 = String::from("Tails positions: ");
+    out1.push_str(&tails_pos_len.to_string());
+    println!("{}",out1);
 }
