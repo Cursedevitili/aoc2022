3b8aba685e71ad0c90b7fcaab495109fc0ec8d82
Cursedevitili <51824354+Cursedevitili@users.noreply.github.com>
2022-12-11 23:01:24 +0200

    Added some day 11 part 2 stuff.

--- src/bin/day11.py
+++ src/bin/day11.py
@@ -83,11 +83,12 @@ def monkey7_test(val: int):
 
 
 class Monkey:
-    def __init__(self, starting_items: list[int], operation: callable, test: callable):
+    def __init__(self, starting_items: list[int], operation: callable, test: callable, limit_worry: bool):
         self.starting_items = starting_items
         self.operation = operation
         self.test = test
         self.times_inspected = 0
+        self.limit_worry = limit_worry
 
     def get_first_item(self) -> int:
         return self.starting_items.pop(0)
@@ -101,8 +102,13 @@ class Monkey:
 
     def inspect(self, item: int) -> (int, int):
         item_after_op = self.do_operation(item)
-        new_worry_level = int(item_after_op / 3)
+        if self.limit_worry:
+            new_worry_level = int(item_after_op / 3)
+        else:
+            new_worry_level = item_after_op
         monkey_to_throw = self.test(new_worry_level)
+        if not self.limit_worry:
+            pass
         self.times_inspected = self.times_inspected + 1
         return monkey_to_throw, new_worry_level
 
@@ -140,33 +146,45 @@ class Monkeys:
 
 
 def main():
-    monkey0demo = Monkey([79, 98], lambda x: x * 19, monkey0demo_test)
-    monkey1demo = Monkey([54, 65, 75, 74], lambda x: x + 6, monkey1demo_test)
-    monkey2demo = Monkey([79, 60, 97], lambda x: x * x, monkey2demo_test)
-    monkey3demo = Monkey([74], lambda x: x + 3, monkey3demo_test)
+    monkey0demo = Monkey([79, 98], lambda x: x * 19, monkey0demo_test, True)
+    monkey1demo = Monkey([54, 65, 75, 74], lambda x: x + 6, monkey1demo_test, True)
+    monkey2demo = Monkey([79, 60, 97], lambda x: x * x, monkey2demo_test, True)
+    monkey3demo = Monkey([74], lambda x: x + 3, monkey3demo_test, True)
     demo_group = Monkeys([monkey0demo, monkey1demo, monkey2demo, monkey3demo])
 
-    monkey0 = Monkey([66, 71, 94], lambda x: x * 5, monkey0_test)
-    monkey1 = Monkey([70], lambda x: x + 6, monkey1_test)
-    monkey2 = Monkey([62, 68, 56, 65, 94, 78], lambda x: x + 5, monkey2_test)
-    monkey3 = Monkey([89, 94, 94, 67], lambda x: x + 2, monkey3_test)
-    monkey4 = Monkey([71, 61, 73, 65, 98, 98, 63], lambda x: x * 7, monkey4_test)
-    monkey5 = Monkey([55, 62, 68, 61, 60], lambda x: x + 7, monkey5_test)
-    monkey6 = Monkey([93, 91, 69, 64, 72, 89, 50, 71], lambda x: x + 1, monkey6_test)
-    monkey7 = Monkey([76, 50], lambda x: x * x, monkey7_test)
-    real_group = Monkeys([monkey0, monkey1, monkey2, monkey3, monkey4, monkey5, monkey6, monkey7])
+    monkey0demoWorry = Monkey([79, 98], lambda x: x * 19, monkey0demo_test, False)
+    monkey1demoWorry = Monkey([54, 65, 75, 74], lambda x: x + 6, monkey1demo_test, False)
+    monkey2demoWorry = Monkey([79, 60, 97], lambda x: x * x, monkey2demo_test, False)
+    monkey3demoWorry = Monkey([74], lambda x: x + 3, monkey3demo_test, False)
+    demo_groupWorry = Monkeys([monkey0demoWorry, monkey1demoWorry, monkey2demoWorry, monkey3demoWorry])
+
+    # monkey0 = Monkey([66, 71, 94], lambda x: x * 5, monkey0_test, True)
+    # monkey1 = Monkey([70], lambda x: x + 6, monkey1_test, True)
+    # monkey2 = Monkey([62, 68, 56, 65, 94, 78], lambda x: x + 5, monkey2_test, True)
+    # monkey3 = Monkey([89, 94, 94, 67], lambda x: x + 2, monkey3_test, True)
+    # monkey4 = Monkey([71, 61, 73, 65, 98, 98, 63], lambda x: x * 7, monkey4_test, True)
+    # monkey5 = Monkey([55, 62, 68, 61, 60], lambda x: x + 7, monkey5_test, True)
+    # monkey6 = Monkey([93, 91, 69, 64, 72, 89, 50, 71], lambda x: x + 1, monkey6_test, True)
+    # monkey7 = Monkey([76, 50], lambda x: x * x, monkey7_test, True)
+    # real_group = Monkeys([monkey0, monkey1, monkey2, monkey3, monkey4, monkey5, monkey6, monkey7])
 
     for _ in range(20):
         demo_group.inspect_items()
         demo_group.print_state()
     demo_group.print_monkey_business_level()
-
     print()
-    print("Real")
+
     for _ in range(20):
-        real_group.inspect_items()
-        real_group.print_state()
-    real_group.print_monkey_business_level()
+        demo_groupWorry.inspect_items()
+        demo_groupWorry.print_state()
+    demo_groupWorry.print_monkey_business_level()
+
+    # print()
+    # print("Real")
+    # for _ in range(20):
+    #     real_group.inspect_items()
+    #     real_group.print_state()
+    # real_group.print_monkey_business_level()
 
 
 if __name__ == "__main__":
