2e64ac4eb7d08b72c372573b295115fe716cf8c4
Cursedevitili <51824354+Cursedevitili@users.noreply.github.com>
2022-12-14 02:08:50 +0200

    Day 13 still in progress

--- src/bin/day13.py
+++ src/bin/day13.py
@@ -1,131 +1,85 @@
-from typing import Optional
-
-class Arr:
-    def __init__(self, level, upper):
-        self.vals = []
-        self.level = level
-        self.upper: Arr = upper
-        self.count = 0
-
-    def add_value(self, val):
-        self.vals.append(val)
-
-    def add_arr(self, new):
-        self.vals.append(new)
-
-    def get_upper(self) -> "Arr":
-        return self.upper
-
-    def get_next(self):
-        if self.count < len(self.vals):
-            ret_val = self.vals[self.count]
-            if isinstance(ret_val, Arr):
-                ret_val = ret_val.get_next()
-            else:
-                self.count = self.count + 1
-            return ret_val
-        elif self.upper != None:
-            self.upper.count = self.upper.count + 1
-            return self.upper.get_next()
-        else:
-            return None
-
-    def get_deepest(self):
-        if len(self.vals) > 0:
-            retval = self.vals[0]
-            if isinstance(retval, Arr):
-                return retval.get_deepest()
-        else:
-            return self.level
-
-
-class Comparator:
-    def __init__(self, left: str, right: str):
-        self.left_string: str = left[1:-1]
-        self.right_string: str = right[1:-1]
-        self.left = Arr(0, None)
-        self.right = Arr(0, None)
-        self.count_left = 0
-
-    def comp_next(self):
-        pass
-
-    def get_vals(self, use_left = True):
-        if use_left:
-            parse_string = self.left_string
-            curr = self.left
-        else:
-            parse_string = self.right_string
-            curr = self.right
-        val_str = ""
-        for c in parse_string:
-            if c == "[":
-                new = Arr(curr.level+1, curr)
-                curr.add_arr(new)
-                curr = new
-            if c == "]":
-                if val_str != "":
-                    val = int(val_str)
-                    curr.add_value(val)
-                    val_str = ""
-                curr = curr.get_upper()
-            if c.isnumeric():
-                val_str = val_str+c
-            if c == "," and val_str != "":
+def get_vals(parse_string: str):
+    ref_arr: list
+    arr = []
+    ref_arr = [arr]
+    i = 0
+    val_str = ""
+    for c in parse_string:
+        if c == "[":
+            new_ref = []
+            ref_arr[i].append(new_ref)
+            ref_arr.append(new_ref)
+            i = i + 1
+        if c == "]":
+            if val_str != "":
                 val = int(val_str)
-                curr.add_value(val)
+                ref_arr[i].append(val)
                 val_str = ""
+            ref_arr.pop()
+            i = i - 1
+        if c.isnumeric():
+            val_str = val_str + c
+        if c == "," and val_str != "":
+            val = int(val_str)
+            ref_arr[i].append(val)
+            val_str = ""
 
-    def compare_pair(self) -> bool:
-        comp_round = 0
-        while 1:
-            comp_round = comp_round + 1
-            comp_left = self.left.get_next()
-            comp_right = self.right.get_next()
-            if comp_left == None and comp_right == None and comp_round == 1:
-                left = self.left.get_deepest()
-                right = self.right.get_deepest()
-                return left < right
+    return arr
 
-            if comp_left == None or comp_right == None:
-                if comp_left == None and comp_right == None:
-                    return True
-                return comp_left == None
-            if type(comp_left) == int and type(comp_right) == int:
-                if comp_left == comp_right:
-                    continue
-                else:
-                    return comp_left < comp_right
 
+def comp_pair(left_list: list, right_list: list, left_def = None, right_def = None ) -> bool:
+    if len(left_list) < len(right_list):
+        lenght = len(left_list)
+    elif len(left_list) == len(right_list):
+        lenght = len(left_list)
+
+    for i in range(lenght):
+        if left_def != None:
+            left = left_def
+        else:
+            left = left_list[i]
+
+        if right_def != None:
+            right = right_def
+        else:
+            right = left_list[i]
+
+        if type(left) == list or type(right) == list:
+            comp_pair(left, right)
+        elif type(left) == list and right_def != None:
+            comp_pair(left, right)
+        elif left_def != None and type(right) == list:
+            comp_pair(left, right)
+        elif type(left_list[i]) == int and type(right_list[i]) == int and left_list[i] != left_list[i]:
+            return left_list[i] < left_list[i]
+        elif left_def != None and right_def != None and left_def == right_def:
+            return True
+
+    raise Exception
 
 
 def main():
-    with open("../../input/day13.txt") as f:
+    with open("../../input/day13demo.txt") as f:
         lines = f.readlines()
-
+    vals = []
     itertor = iter(lines)
-    comps: [Comparator] = []
     while 1:
         left = next(itertor)
-        print(left)
+        # print(left)
+        vals.append(get_vals(left[1:-1]))
         right = next(itertor)
-        print(right)
-        comps.append(Comparator(left, right))
+        # print(right)
+        vals.append(get_vals(right[1:-1]))
         stop = next(itertor, None)
         if stop == None:
             break
 
     right_pairs = []
-    for i, pair in enumerate(comps):
-        pair.get_vals(use_left=True)
-        pair.get_vals(use_left=False)
-        result = pair.compare_pair()
-        if result:
-            right_pairs.append(i+1)
+    for i in range(0, len(vals), 2):
+        comp_pair(vals[i], vals[i+1])
 
     print(right_pairs)
     print(f"Sum of these is: {sum(right_pairs)}")
-
 
 
 if __name__ == "__main__":
